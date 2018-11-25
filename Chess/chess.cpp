5792fb386e81b2ff0c267ebb086beb2b98ff5b87
hdubf <flyme4135@gmail.com>
2018-11-25 23:18:01 +0800

    add

---
+++ Chess/chess.cpp
@@ -0,0 +1,428 @@
+﻿#include <graphics.h>
+#include <conio.h>
+#include "chess.h"
+
+chessNode* g_redChessListHead;/* 红棋链表 */
+chessNode* g_blackChessListHead;/* 黑棋链表 */
+wchar_t g_blackChessText[] = { L'車',L'馬',L'象',L'士',L'將',L'砲',L'卒' };
+wchar_t g_redChessText[] = { L'俥',L'傌',L'相',L'仕',L'帥',L'炮',L'兵' };
+int g_status = RED_TAG;
+int count = 1;
+
+/* 棋子链表 */
+chessNode *InitList()
+{
+	chessNode *head = (chessNode*)malloc(sizeof(chessNode));/* 申请存储空间，得到头结点 */
+	head->next = NULL;
+	return head;
+}
+/* 增加一个棋子 */
+void ListInsert(chessNode *head, CHESS *chess)
+{
+	chessNode *temp = head;
+	while (temp->next != NULL)
+	{
+		temp = temp->next;
+	}
+	chessNode *t = (chessNode*)malloc(sizeof(chessNode));
+	t->chess = chess;
+	t->next = temp->next;
+	temp->next = t;
+}
+/* 删除一个棋子 */
+void ListDelete(chessNode *head, chessNode *node)
+{
+	chessNode *temp = head;
+	chessNode *t;
+	while (temp->next != NULL)
+	{
+		if (temp->next == node) break;
+		temp = temp->next;
+	}
+	if (!temp->next) return;
+	t = temp->next;
+	temp->next = t->next;
+	free(t);
+}
+/* 画出一个红棋 */
+void DrawRedChess(CHESS *chess)
+{
+	circle(chess->x, chess->y, CHESS_RADIUS);
+	moveto(chess->x - GRID_WIDTH / 4, chess->y - GRID_WIDTH / 5);
+	settextcolor(RED);
+	settextstyle(30, 0, _T("宋体"));
+	outtext(g_redChessText[chess->index]);
+	
+	/*switch (tag)
+	{
+	case 0:
+		outtext(_T("俥"));
+		break;
+	case 1:
+		outtext(_T("傌"));
+		break;
+	case 2:
+		outtext(_T("相"));
+		break;
+	case 3:
+		outtext(_T("仕"));
+		break;
+	case 4:
+		outtext(_T("帥"));
+		break;
+	case 5:
+		outtext(_T("炮"));
+		break;
+	case 6:
+		outtext(_T("兵"));
+		break;
+	}*/
+}
+/* 画出一个黑棋 */
+void DrawBlackChess(CHESS *chess)
+{
+	circle(chess->x, chess->y, CHESS_RADIUS);
+	moveto(chess->x - GRID_WIDTH / 4, chess->y - GRID_WIDTH / 5);
+	settextcolor(BLACK);
+	settextstyle(30, 0, _T("宋体"));
+	outtext(g_blackChessText[chess->index]);
+	/*switch (tag)
+	{
+	case 0:
+		outtext(_T("車"));
+		strcpy(chess->text, "車");
+		break;
+	case 1:
+		outtext(_T("馬"));
+		strcpy(chess->text, "馬");
+		break;
+	case 2:
+		outtext(_T("象"));
+		strcpy(chess->text, "象");
+		break;
+	case 3:
+		outtext(_T("士"));
+		strcpy(chess->text, "士");
+		break;
+	case 4:
+		outtext(_T("將"));
+		strcpy(chess->text, "將");
+		break;
+	case 5:
+		outtext(_T("砲"));
+		strcpy(chess->text, "砲");
+		break;
+	case 6:
+		outtext(_T("卒"));
+		strcpy(chess->text, "卒");
+		break;
+	}*/
+}
+/* 创建黑棋 */
+void MakeBlackChess(CHESS *chess, int x, int y,int tag)
+{
+	chess = (CHESS*)malloc(sizeof(CHESS));
+	chess->x = x;
+	chess->y = y;
+	chess->index = tag;
+	DrawBlackChess(chess);
+	ListInsert(g_blackChessListHead, chess);
+}
+/* 创建红棋 */
+void MakeRedChess(CHESS *chess, int x, int y,int tag)
+{
+	chess = (CHESS*)malloc(sizeof(CHESS));
+	chess->x = x;
+	chess->y = y;
+	chess->index = tag;
+	DrawRedChess(chess);
+	ListInsert(g_redChessListHead, chess);
+}
+
+/* 寻找被点击的棋子 */
+chessNode *FindChessNode(int x, int y,int tag)
+{
+	chessNode *temp;
+	int dr;
+	if(tag==RED_TAG) temp = g_redChessListHead;
+	else if (tag == BLACK_TAG) temp = g_blackChessListHead;
+	else return NULL;
+	while ((temp = temp->next) != NULL)
+	{
+		dr = (x - temp->chess->x)*(x - temp->chess->x) + (y - temp->chess->y)*(y - temp->chess->y) - CHESS_RADIUS * CHESS_RADIUS;
+		if (dr <= 0) return temp;
+	}
+	return NULL;
+}
+
+/* 画出链表中的所有棋子 */
+void DrawAllChess(chessNode *red, chessNode *black)
+{
+	chessNode *tempRed = red;
+	chessNode *tempBlack = black;
+	BeginBatchDraw();
+	cleardevice();
+	DrawBoard();
+	while ((tempBlack = tempBlack->next) != NULL) DrawBlackChess(tempBlack->chess);
+	while ((tempRed = tempRed->next) != NULL) DrawRedChess(tempRed->chess);
+	FlushBatchDraw();
+}
+/* 移动棋子 */
+void MoveChess(chessNode* node,int x, int y,int colorTag)
+{
+	CHESS *chess = NULL;
+	chessNode *destinationChess = NULL;
+	int preX = node->chess->x;
+	int preY = node->chess->y;
+	int min, max;
+	int i = 0;
+	int chessNum;
+	x = CalCoordinate(x-GRID_WIDTH, y-GRID_WIDTH)[0];
+	y = CalCoordinate(x-GRID_WIDTH, y-GRID_WIDTH)[1];
+	switch (node->chess->index)
+	{
+	case 0:/* 车 */
+		if (!(y == preY) && !(x == preX))/* 只能走直线 */
+			return;
+		break;
+	case 1:/* 马 */
+		if (!(abs(x - preX) == GRID_WIDTH && abs(y - preY) == 2*GRID_WIDTH)&&
+			!(abs(x - preX) == 2*GRID_WIDTH && abs(y - preY) == GRID_WIDTH))
+			return;
+		break;
+	case 2:/* 象 */
+		break;
+	case 3:/* 士 */
+		if (!InPalace(x, y)) return;
+		if (!(abs(x - preX) == GRID_WIDTH && abs(y - preY) == GRID_WIDTH) &&
+			!(abs(x - preX) == GRID_WIDTH && abs(y - preY) == GRID_WIDTH))/* 只能走对角 */
+			return;
+		break;
+	case 4:/* 将 */
+		if (!InPalace(x, y)) return;
+		if ((abs(x - preX) == GRID_WIDTH && y == preY) ||
+			(abs(y - preY) == GRID_WIDTH && x == preX))/* 只走一步 */
+			switch (g_status)
+			{
+			case RED_TAG:
+				if (destinationChess = FindChessNode(x, y, BLACK_TAG)) Eat(destinationChess);
+				break;
+			case BLACK_TAG:
+				if (destinationChess = FindChessNode(x, y, RED_TAG)) Eat(destinationChess);
+				break;
+			}
+		break;
+	case 5:/* 炮 */
+		if (!(y == preY) && !(x == preX))/* 只能走直线 */
+			return;
+		if ((abs(x - preX) == GRID_WIDTH && y == preY) ||
+			(abs(y - preY) == GRID_WIDTH && x == preX))/* 只走一步 */
+			goto move;
+		switch (g_status)
+		{
+		case RED_TAG:
+			if (!(destinationChess=FindChessNode(x, y, BLACK_TAG))) return;
+			break;
+		case BLACK_TAG:
+			if (!(destinationChess=FindChessNode(x, y, RED_TAG))) return;
+			break;
+		}
+		chessNum = 0;
+		if (y == preY)
+		{
+			min = MIN(preX, x);
+			max = MAX(preX, x);
+			for (i = min + GRID_WIDTH; i < max; i += GRID_WIDTH)
+			{
+				if (FindChessNode(i, y, BLACK_TAG) || FindChessNode(i, y, RED_TAG)) 
+					chessNum++;
+			}
+			if (chessNum != 1) return;
+		}
+		if (x == preX)
+		{
+			min = MIN(preY, y);
+			max = MAX(preY, y);
+			for (i = min + GRID_WIDTH; i < max; i += GRID_WIDTH)
+			{
+				if (FindChessNode(x, i, BLACK_TAG) || FindChessNode(x, i, RED_TAG))
+					chessNum++;
+			}
+			if (chessNum != 1) return;
+		}
+		Eat(destinationChess);
+		break;
+	case 6:/* 兵 */
+		switch (g_status)
+		{
+		case RED_TAG:
+			if (!(abs(x - preX) == GRID_WIDTH && y == preY) &&
+				!(y - preY == -GRID_WIDTH && x == preX))
+				return;
+			break;
+		case BLACK_TAG:
+			if (!(abs(x - preX) == GRID_WIDTH && y == preY) &&
+				!(y - preY == GRID_WIDTH && x == preX))
+				return;
+			break;
+		}
+		if (destinationChess = FindChessNode(x, y, BLACK_TAG)) Eat(destinationChess);
+		break;
+	}
+	move:
+	if (colorTag == RED_TAG) 
+	{
+		MakeRedChess(chess, x, y, node->chess->index);/* 在指定位置画一个棋子 */
+		ListDelete(g_redChessListHead, node);/* 删除原先的棋子 */
+		count++;
+	}
+	else if (colorTag == BLACK_TAG)
+	{
+		MakeBlackChess(chess, x, y, node->chess->index);/* 在指定位置画一个棋子 */
+		ListDelete(g_blackChessListHead, node);/* 删除原先的棋子 */
+		count++;
+	}
+	DrawAllChess(g_redChessListHead, g_blackChessListHead);/* 画出所有棋子 */
+	return;
+}
+/* 初始化 */
+void Setup()
+{
+	int i;
+	CHESS *chess = NULL;
+	g_redChessListHead = InitList();
+	g_blackChessListHead = InitList();
+	initgraph(WIDTH, HEIGHT);
+	setbkcolor(WHITE);
+	settextcolor(BLACK);
+	setlinecolor(BLACK);
+	cleardevice();
+
+	DrawBoard();
+	/* 黑棋 */
+	for (i = 0; i < 9; i++)
+	{
+		if(i<=4) MakeBlackChess(chess, (i + 1) * GRID_WIDTH, GRID_WIDTH, i);
+		else MakeBlackChess(chess, (i + 1) * GRID_WIDTH, GRID_WIDTH, 8-i);
+	}
+	MakeBlackChess(chess, 2 * GRID_WIDTH, 3 * GRID_WIDTH, 5);
+	MakeBlackChess(chess, 8 * GRID_WIDTH, 3 * GRID_WIDTH, 5);
+	for (i = 0; i < 9; i += 2)
+		MakeBlackChess(chess, (i + 1)*GRID_WIDTH, 4 * GRID_WIDTH, 6);
+	/* 红棋 */
+	for (i = 0; i < 9; i += 2)
+		MakeRedChess(chess, (i + 1)*GRID_WIDTH, 7 * GRID_WIDTH, 6);
+	MakeRedChess(chess, 2 * GRID_WIDTH, 8 * GRID_WIDTH,5);
+	MakeRedChess(chess, 8 * GRID_WIDTH, 8 * GRID_WIDTH,5);
+	for (i = 0; i < 9; i++)
+	{
+		if (i <= 4) MakeRedChess(chess, (i + 1)*GRID_WIDTH, GRID_WIDTH + BOARD_HEIGHT,i);
+		else MakeRedChess(chess, (i + 1)*GRID_WIDTH, GRID_WIDTH + BOARD_HEIGHT, 8-i);
+	}
+}
+/* 开始游戏 */
+void StartUp()
+{
+	MOUSEMSG m;
+	chessNode *currentChess = NULL;
+	chessNode *previousChess = NULL;
+	int chessAvailable = FALSE;
+
+	while (TRUE)
+	{
+		if (count % 2 == 1) g_status = RED_TAG;
+		else g_status = BLACK_TAG;
+		m = GetMouseMsg();
+		if (m.uMsg==WM_LBUTTONDOWN)
+		{
+			currentChess = FindChessNode(m.x, m.y, g_status);
+			if (currentChess != NULL)
+				chessAvailable = TRUE;
+			else if (chessAvailable&&previousChess) 
+				MoveChess(previousChess, m.x, m.y,g_status);
+			previousChess = currentChess;
+		}
+		Sleep(10);
+	}
+}
+/* 画出棋盘 */
+void DrawBoard()
+{
+	int i, j;
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 9; j++)
+		{
+			if (j == 4) continue;
+			rectangle((i + 1)*GRID_WIDTH, (j + 1)*GRID_WIDTH, (i + 2)*GRID_WIDTH, (j + 2)*GRID_WIDTH);
+		}
+	}
+}
+
+int *CalCoordinate(int x, int y)
+{
+	int temp[4];
+	int result[2];
+	int x_value[4];
+	int y_value[4];
+	int minDr;
+	int i;
+
+	x_value[0] = (x / 60)*GRID_WIDTH;
+	y_value[0] = (y / 60)*GRID_WIDTH;
+	temp[0] = (x_value[0] - x)*(x_value[0] - x) + (y_value[0] - y)*(y_value[0] - y);
+
+	x_value[1] = (x_value[0]/ GRID_WIDTH + 1)*GRID_WIDTH;
+	y_value[1] = (y_value[0] / GRID_WIDTH) *GRID_WIDTH;
+	temp[1] = (x_value[1] - x)*(x_value[1] - x) + (y_value[1] - y)*(y_value[1] - y);
+
+
+	x_value[2] = (x_value[0]/ GRID_WIDTH) *GRID_WIDTH;
+	y_value[2] = (y_value[0] / GRID_WIDTH + 1)*GRID_WIDTH;
+	temp[2] = (x_value[2] - x)*(x_value[2] - x) + (y_value[2] - y)*(y_value[2] - y);
+
+	x_value[3] = (x_value[0] / GRID_WIDTH + 1)*GRID_WIDTH;
+	y_value[3] = (y_value[0] / GRID_WIDTH + 1)*GRID_WIDTH;
+	temp[3] = (x_value[3] - x)*(x_value[3] - x) + (y_value[3] - y)*(y_value[3] - y);
+
+	minDr = MIN(MIN(MIN(temp[0], temp[1]), temp[2]), temp[3]);
+	for (i = 0; i < 4;i++) 
+	{
+		if (temp[i] == minDr) 
+		{
+			result[0] = x_value[i] + GRID_WIDTH;
+			result[1] = y_value[i] + GRID_WIDTH;
+			return result;
+		}
+	}
+	return NULL;
+}
+
+int InPalace(int x, int y)
+{
+	switch (g_status)
+	{
+	case RED_TAG:
+		if(IN_RED_PALACE(x,y))
+			return TRUE;
+		else
+			return FALSE;
+	case BLACK_TAG:
+		if (IN_BLACK_PALACE(x, y))
+			return TRUE;
+		else
+			return FALSE;
+	}
+}
+void Eat(chessNode* node)
+{
+	switch (g_status)
+	{
+	case RED_TAG:
+		ListDelete(g_blackChessListHead, node);
+		break;
+	case BLACK_TAG:
+		ListDelete(g_redChessListHead, node);
+		break;
+	}
+}
---
+++ Chess/chess.h
@@ -0,0 +1,58 @@
+#include <graphics.h>
+#include <conio.h>
+#include <math.h>
+
+#define MIN(a,b) a > b ? b : a
+#define MAX(a,b) a > b ? a : b
+#define IN_RED_PALACE(x,y) (x>=4*GRID_WIDTH&&x<=6*GRID_WIDTH&&y>=8*GRID_WIDTH&&y<=10*GRID_WIDTH)
+#define IN_BLACK_PALACE(x,y) (x>=4*GRID_WIDTH&&x<=6*GRID_WIDTH&&y>=GRID_WIDTH&&y<=3*GRID_WIDTH)
+
+#define GRID_WIDTH 60
+#define WIDTH GRID_WIDTH * 10
+#define HEIGHT GRID_WIDTH * 11
+#define BOARD_WIDTH GRID_WIDTH * 8
+#define BOARD_HEIGHT GRID_WIDTH * 9
+#define CHESS_RADIUS GRID_WIDTH / 2 - 4
+
+
+
+#define RED_TAG 1
+#define BLACK_TAG 2
+
+#define FALSE 0
+#define TRUE 1
+
+typedef struct chessTag
+{
+	int x;
+	int y;
+	int index;
+}CHESS;
+typedef struct chessList
+{
+	CHESS *chess;
+	chessList *next;
+}chessNode;
+
+void Setup();/* »­³öÆåÅÌ */
+
+void MakeBlackChess(CHESS *, int, int ,int);
+
+void MakeRedChess(CHESS *, int, int,int);
+
+chessNode *FindChessNode(int, int, int);
+void DrawAllChess(chessNode *, chessNode *);
+
+chessNode *InitList();
+
+void ListInsert(chessNode *,CHESS *);
+
+void StartUp();
+
+void MoveChess(chessNode* node, int x, int y, int colorTag);
+void DrawRedChess(CHESS *chess);
+void DrawBlackChess(CHESS *chess);
+void DrawBoard();
+int *CalCoordinate(int x,int y);
+int InPalace(int x, int y);
+void Eat(chessNode* node);
